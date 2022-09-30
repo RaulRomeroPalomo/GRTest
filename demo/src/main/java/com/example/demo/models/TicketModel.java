d1f4efd9d3cf56bd92747b31033c74ed92dc6550
SGND <rromero@gmail.com>
2022-09-30 12:20:21 +0200

    Añadir proyecto a git

---
+++ demo/src/main/java/com/example/demo/models/TicketModel.java
@@ -0,0 +1,62 @@
+package com.example.demo.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ticket")
+public class TicketModel {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+    private Long id;
+    private Date creationDate;
+    private Integer totalAmount;
+    private Long lineIdentifier;
+    private String description;
+    private Integer amount;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+    public Long getLineIdentifier() {
+        return lineIdentifier;
+    }
+    public void setLineIdentifier(Long lineIdentifier) {
+        this.lineIdentifier = lineIdentifier;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+}
